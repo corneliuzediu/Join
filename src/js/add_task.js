c154b3d980232c5a296ad28492bdc0bf5724ea6f
corneliuzediu <zediucorneliu@gmail.com>
2023-02-13 08:21:53 +0100

    Repeared create task animation

--- src/js/add_task.js
+++ src/js/add_task.js
@@ -346,6 +346,7 @@ function readSubtasks() {
  * @returns a function call and clears all input fields
  */
 async function createTask(path) {
+  document.getElementById('task-added').classList.remove('d-none')
   let title = document.getElementById("title").value;
   let taskID = new Date().getTime();
   let contactsCheckedBoxes = getCheckedBoxes("assign-contacts");
